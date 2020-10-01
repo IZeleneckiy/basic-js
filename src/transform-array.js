a19db5d211841b342c42a41023c1bbe834fd3eaa
Ivan Zeleneckiy <izeleneckiy@gmail.com>
2020-10-01 14:35:22 +0400

    feat: implement transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+  let result = arr.slice();
+  for (let i = 0; i < result.length; i++) {
+    switch (result[i]) {
+      case '--discard-next':
+        if ((i + 1) < result.length) {
+          result.splice(i + 1, 1);
+        }
+        break;
+      case '--double-next':
+        if (i + 1 < result.length) {
+          result.splice(i + 1, 0, result[i + 1]);
+        }
+        break;
+      case '--discard-prev':
+        if (i - 1 >= 0) {
+          result.splice(i -1, 1);
+          i--;
+        }
+        break;
+      case '--double-prev':
+        if (i - 1 >= 0) {
+          result.splice(i - 1, 0, result[i - 1]);
+          i++;
+        }
+        break;
+    }
+  }
+  return result.filter(a => a!== '--discard-next' && a!== '--double-next' && a!== '--double-prev' && a!== '--discard-prev');
 };
