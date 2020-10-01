aaa424db644e9be7ae804548b46710671ae2225a
Ivan Zeleneckiy <izeleneckiy@gmail.com>
2020-10-01 11:04:02 +0400

    refactor:

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,12 +4,9 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if ((typeof sampleActivity === "string") &&
+  return (typeof sampleActivity === "string") &&
       !(+sampleActivity !== +sampleActivity) &&
       !(sampleActivity > +MODERN_ACTIVITY) &&
-      (+sampleActivity > 1)) {
-    let result = (Math.log(+MODERN_ACTIVITY - sampleActivity) / (0.693 / +HALF_LIFE_PERIOD));
-    console.log(`sampleActivity = ${sampleActivity}; result = ${result}; +sampleActivity = ${+sampleActivity}`);
-    return result;
-  } else return false;
+      (+sampleActivity > 1) &&
+      (Math.log(+MODERN_ACTIVITY - sampleActivity) / (0.693 / +HALF_LIFE_PERIOD));
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,13 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(array) {
-  let counter = 0;
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array[i].length; j++) {
-      if (array[i][j] === '^^') {
-        counter++;
-      }
-    }
-  }
-  return counter;
-};
+  return  array.length !== 0 && array.reduce((a, b) => a.concat(b)).filter(a => a === '^^').length;
+}
