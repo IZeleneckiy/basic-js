dc341674e5cd86baf274128b5fbd06e9596eaab9
izeleneckiy <izeleneckiy@gmail.com>
2021-03-10 22:17:02 +0300

    fix: change carbon-dating solution again

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -7,6 +7,6 @@ module.exports = function dateSample(sampleActivity) {
   return (typeof sampleActivity === "string") &&
       !(+sampleActivity !== +sampleActivity) &&
       !(sampleActivity > MODERN_ACTIVITY) &&
-      (parseFloat(sampleActivity) > 1) &&
+      (parseFloat(sampleActivity) > 0) &&
       (Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (Math.LN2 / HALF_LIFE_PERIOD)));
 };
